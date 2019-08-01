d33224bd18beae231741ec824468db17c1325aaa
Abylaikhan Zulbukharov <l1pt0n1905@gmail.com>
2019-08-01 15:15:15 +0600

    [save state][redux]

--- redux-sandbox/README.md
+++ redux-sandbox/README.md
@@ -8,3 +8,61 @@
 
 > если action undefined => return state
 
+## Store
+
+> Store координирует работу с данными в Redux приложении
+
+```
+const store = createStore(reducer);
+```
+
+> Получение нотификации об изменениях
+
+```
+store.subscribe(() => {
+	//do smth
+})
+```
+
+> Обработать новый action
+
+```
+store.dispatch(action);
+```
+## UI для Redux
+
+> В качестве UI может использоваться любая библиотека или фреймворк
+
+> store.dispatch() используется для обновления состояния
+
+> store.subscripe() используется для обновления UI
+
+## ACTION
+
+> Кроме типа, action может содержать дополнительную информацию
+
+```
+store.dispatch(
+	{
+		type: 'USER_LOGGED_IN',
+		name: 'Arnold',
+		role: 'Admin',
+	}
+);
+```
+
+## ACTION CREATOR
+
+> функция, которая создает объекты action
+
+```
+const userLoggedIn = (name, role) => {
+	return ({
+		type: 'USER_LOGGED_IN',
+		name,
+		role,
+	})
+};
+
+store.dispatch(userLoggedIn('Arnold', 'Admin'));
+```
--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,16 +1,48 @@
-// счетчик
-const reducer = (state = 0, action) => {
-	switch (action.type) {
-		case 'INC':
-			return state + 1;
-		default:
-			return state;
-	}
+import { createStore } from 'redux';
+import { dec, inc, rnd } from './actions';
+import reducer from './reducer';
+
+const store = createStore(reducer);
+
+document
+	.getElementById('inc')
+	.addEventListener('click', () => {
+		store.dispatch(inc());
+	})
+
+document
+	.getElementById('dec')
+	.addEventListener('click', () => {
+		store.dispatch(dec());
+	})
+
+document
+	.getElementById('rnd')
+	.addEventListener('click', () => {
+		const payload = Math.floor(Math.random() * 10);
+		store.dispatch(rnd(payload));
+	})
+
+const update = () => {
+	document
+		.getElementById('counter')
+		.innerHTML = store.getState();
 };
 
-let state = reducer(undefined, {});
+store.subscribe(() => {
+	update();
+});
 
-state = reducer(state, { type: 'INC' });
-console.log(state);
-state = reducer(state, { type: 'INC' });
-console.log(state);
+
+// при любом изменении вызывать функцию
+// store.subscribe(() => {
+	// console.log(store.getState());
+// })
+
+// console.log(store.getState());
+// getState() получение текущего state
+
+// просто вызов reducer
+// store.dispatch({ type: 'INC' });
+// store.dispatch({ type: 'INC' });
+// console.log(store.getState());
