189a8ddd869f257c2af07c08999bb152da4a4930
Zulbukharov <l1pt0n1905@gmail.com>
2019-08-08 18:54:14 +0600

    []

--- chat/web/src/components/app/app.js
+++ chat/web/src/components/app/app.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import GithubService from '../../services/github-service';
 import Header from '../header';
 import './app.css'
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
 
 export default class App extends Component {
 	githubService = new GithubService();
@@ -17,6 +21,30 @@ export default class App extends Component {
 	//         data: user,
 	//     })
 	// }
+
+	createApolloClient = (authToken) => {
+
+		return new ApolloClient({
+
+			link: new HttpLink({
+
+				uri: 'http://testing-app-1997.herokuapp.com/v1alpha1/graphql',
+
+				headers: {
+
+					Authorization: `Bearer ${authToken}`
+
+				}
+
+			}),
+
+			cache: new InMemoryCache(),
+
+		});
+
+	};
+
+
 
 	componentDidMount() {
 		// const url = new URL(window.location.href);
@@ -34,7 +62,8 @@ export default class App extends Component {
 
 	render() {
 		const { data: { avatar_url, bio, login } } = this.state;
-
+		const jwt_token = localStorage.getItem('jwt')
+		const client = this.createApolloClient(jwt_token)
 		return (
 			<React.Fragment>
 				<Header logout={this.props.auth.logout} />
